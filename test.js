f3d3464d517ca2b888d2114be580fdff37deae24
testtlnls <41869011+testtlnls@users.noreply.github.com>
2019-12-17 07:03:32 +0900

    Update test.js

--- test.js
+++ test.js
@@ -160,46 +160,12 @@ function buildDisplayTable(responseData){
 				document.getElementById('resultTableTr'+i).appendChild(obj);
 			};
 };
-/* Reference of following function bin2hex(buffer) is http://code.i-harness.com/ja/docs/dom/subtlecrypto/digest */
-
-/* function bin2hex(buffer) {
-  var hexCodes = [];
-  var view = new DataView(buffer);
-  for (var i = 0; i < view.byteLength; i += 4) {
-    /* Using getUint32 reduces the number of iterations needed (we process 4 bytes each time) */
-    var value = view.getUint32(i)
-    /* toString(16) will give the hex representation of the number without padding */
-    var stringValue = value.toString(16)
-    /* We use concatenation and slice for padding */
-    var padding = '00000000'
-    var paddedValue = (padding + stringValue).slice(-padding.length)
-    hexCodes.push(paddedValue);
-  };
-
-  /* Join all the hex strings into one */
-  return hexCodes.join("");
-}; */
-
 function bin2hex(buffer) {
-  return Array.from(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+	return Array.from(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
 };
-
-/* Reference of following function _base64ToArrayBuffer(base64) is unknown. */
-/* If you use this for public use, please replace this function. */
-/* function _base64ToArrayBuffer(base64) {
-    var binary_string =  window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array( len );
-    for (var i = 0; i < len; i++)        {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes.buffer;
-}; */
-
 function _base64ToArrayBuffer(base64) {
 	return Uint8Array.from(window.atob(base64).split(''), x => x.charCodeAt(0)).buffer;
 };
-
 document.getElementById('cert').onclick=function(){
 	var pem = document.getElementById('certInput').value;
 	pem = pem.replace('-----BEGIN CERTIFICATE-----\n','');
